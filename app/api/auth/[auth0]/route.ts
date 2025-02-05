a85f8ffaca5a94604c932d77eaa8ab9b17449388
starla huang <starla_huang@epam.com>
2025-02-05 16:22:59 +0800

    auth0

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,13 +1,17 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0'
+import { AfterCallbackAppRoute, handleAuth, handleCallback, handleLogin } from '@auth0/nextjs-auth0'
+import { NextResponse } from 'next/server'
 
+const afterCallback: AfterCallbackAppRoute = async (req, session) => {
+  return session
+}
 export const GET = handleAuth({
-  login: handleLogin({
-    returnTo: '/dashboard'
-  }),
-  signup: handleLogin({
-    authorizationParams: {
-      screen_hint: 'signup'
-    },
-    returnTo: '/dashboard'
-  })
+  callback: async (req: any, ctx: any) => {
+    try {
+      // you need to cast this as returning a response because the auth handlers support
+      // signatures for both routers
+      return await handleCallback(req, ctx, { afterCallback })
+    } catch (error) {
+      return NextResponse.redirect('/auth/error')
+    }
+  }
 })
