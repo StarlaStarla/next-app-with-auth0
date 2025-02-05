a155a35e213f7d89ccd0e4da57a850564a6b14aa
starla huang <starla_huang@epam.com>
2025-02-05 17:10:35 +0800

    login

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,25 +1,3 @@
-import { handleAuth, handleCallback, Session } from '@auth0/nextjs-auth0'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { handleAuth } from '@auth0/nextjs-auth0'
 
-const afterCallback = async (req: NextApiRequest, res: NextApiResponse, session: Session) => {
-  console.log('I am getting called after callback', session)
-  if (session.user) {
-    res.status(200).redirect('/dashboard')
-    console.log('user found', session.user)
-  } else {
-    console.log('user not found', session)
-  }
-
-  return session
-}
-export const GET = handleAuth({
-  callback: async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-      // you need to cast this as returning a response because the auth handlers support
-      // signatures for both routers
-      return await handleCallback(req, res, { afterCallback })
-    } catch (error) {
-      console.error('error----------------------', error)
-    }
-  }
-})
+export const GET = handleAuth()
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+'use client'
 import './globals.css'
 import { useUser } from '@auth0/nextjs-auth0/client'
 import Image from 'next/image'
-import Login from './login/page'
-import Page from './dashboard/page'
+import Link from 'next/link'
 
 export default function Home() {
   const loadingImg = 'https://cdn.auth0.com/blog/hello-auth0/loader.svg'
@@ -15,8 +15,29 @@ export default function Home() {
     )
   }
   if (!user) {
-    return <Login></Login>
+    return (
+      <main>
+        <Link href='/api/auth/login?screen_hint=signup'>
+          <button>Sign up</button>
+        </Link>
+        <Link href='/api/auth/login'>
+          <button>Log in</button>
+        </Link>
+      </main>
+    )
   }
 
-  return <Page></Page>
+  return (
+    <main>
+      <h1>
+        Welcome, {user.name}
+        {user.email}!
+      </h1>
+      <p>
+        <Link href='/api/auth/logout'>
+          <button>Log out</button>
+        </Link>
+      </p>
+    </main>
+  )
 }
