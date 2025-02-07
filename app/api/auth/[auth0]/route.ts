68dfa65aab6a3239bb468b43d3666f588a93f16f
starla huang <starla_huang@epam.com>
2025-02-07 15:29:01 +0800

    route

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,15 +1,7 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0'
-import { NextApiRequest, NextApiResponse } from 'next'
-console.log('the AUTH0_SECRET env var is set: ', process.env.AUTH0_SECRET)
 
 export const GET = handleAuth({
-  login: async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-      return await handleLogin(req, res, {
-        returnTo: '/dashboard'
-      })
-    } catch (error) {
-      console.error('handleAuth-----', error)
-    }
-  }
+  login: handleLogin({
+    returnTo: '/dashboard'
+  })
 })
